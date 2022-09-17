02ad8a1df6b6180c4df9d6d513b180a17f7ec35e
ChrisKoetzee <102851549+ChrisKoetzee@users.noreply.github.com>
2022-09-17 17:26:38 +0200

    level 200

--- script.js
+++ script.js
@@ -7,33 +7,21 @@ function setup() {
 }
 
 let container = document.querySelector('#root');
-let searchBar = document.createElement("input");
+let searchBar = document.querySelector("input");
 
-  console.log(searchBar + "is my searchBar");
-  console.log(container + "is my holder");
+  // console.log(searchBar + "is my searchBar");
+  // console.log(container + "is my holder");
 
-  container.appendChild(searchBar);
-  searchBar.setAttribute("type", "text");
-  searchBar.setAttribute("id", "search");
-  searchBar.setAttribute("placeholder", "search for Episode");
-  document.getElementById("search").style.zIndex = "1";
-  document.getElementById("search").style.position = "fixed";
-  document.getElementById("search").style.margin = "auto";
   
-  console.log(searchBar);
-
-  // searchBar.addEventListener("keyup", (e) => {
-  //   console.log(searchBar);
-    // const searchInput = e.target.value.toLowerCase();
-    // const filteredEpisodes = container.filter(searchEp => {
-    //   return (
-    //     searchEp.name.toLowerCase().includes(searchInput) || 
-    //     searchEp.summary.toLowerCase().includes(searchInput)
-    //     );
-    // })
-    // console.log(getAllEpisodes(filteredEpisodes));
-  // })
-
+  document.querySelector("input").setAttribute("type", "text");
+  document.querySelector("input").setAttribute("id", "search");
+  document.querySelector("input").setAttribute("placeholder", "search for Episode");
+  document.getElementById("search").style.zIndex = "1";
+  document.getElementById("search").style.position = "absolute";
+  document.getElementById("search").style.margin = "auto";
+  document.getElementById("search").style.padding = "auto";
+  
+  // console.log(searchBar + "this is my updated searchbar");
 
 function makePageForEpisodes(episodeList) {
 let output = "";
@@ -51,3 +39,22 @@ let output = "";
   container.innerHTML = output;
 }
 window.onload = setup;
+
+console.log(searchBar);
+
+let filtered = Object.keys(getAllEpisodes)
+console.log(filtered + "begining");
+
+searchBar.addEventListener("keyup", (e) => {
+  const searchInput = e.target.key;
+  // .toLowerCase()
+  console.log(e.target.key + "targeted");
+  const filteredEpisodes = filtered.filter(searchEp => {
+    return (
+      searchEp.name.toLowerCase().includes(searchInput) || 
+      searchEp.summary.toLowerCase().includes(searchInput)
+      );
+  })
+  console.log(getAllEpisodes(filteredEpisodes));
+})
+console.log(filtered + "end");
