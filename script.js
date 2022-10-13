af5ef76ae29d033419988db4a9c756a4f4b05f93
ChrisKoetzee <102851549+ChrisKoetzee@users.noreply.github.com>
2022-10-13 07:24:02 +0200

    200 still

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
 //You can edit ALL of the code here
-
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
-  // console.log(getAllEpisodes());
 }
 
 let container = document.querySelector('#root');
-let searchBar = document.querySelector("input");
-
-  // console.log(searchBar + "is my searchBar");
-  // console.log(container + "is my holder");
-
-  
-  document.querySelector("input").setAttribute("type", "text");
-  document.querySelector("input").setAttribute("id", "search");
-  document.querySelector("input").setAttribute("placeholder", "search for Episode");
-  document.getElementById("search").style.zIndex = "1";
-  document.getElementById("search").style.position = "absolute";
-  document.getElementById("search").style.margin = "auto";
-  document.getElementById("search").style.padding = "auto";
-  
-  // console.log(searchBar + "this is my updated searchbar");
 
 function makePageForEpisodes(episodeList) {
 let output = "";
@@ -38,23 +21,37 @@ let output = "";
   }
   container.innerHTML = output;
 }
-window.onload = setup;
+window.onload = setup;  
 
-console.log(searchBar);
+let searchBar = document.querySelector("input");
+let searchInput ;
 
-let filtered = Object.keys(getAllEpisodes)
-console.log(filtered + "beginning");
+searchBar.addEventListener("keyup" ,() => {
+    searchInput = searchBar.value;
+    let result = getSearch(searchInput);
+    console.log(result);
+});
 
-searchBar.addEventListener("keyup", (e) => {
-  const searchInput = e.target.key;
-  // .toLowerCase()
-  console.log(e.target.key + "targeted");
-  const filteredEpisodes = filtered.filter(searchEp => {
-    return (
-      searchEp.name.toLowerCase().includes(searchInput) || 
-      searchEp.summary.toLowerCase().includes(searchInput)
-      );
-  })
-  console.log(getAllEpisodes(filteredEpisodes));
-})
-console.log(filtered + "end");
+
+function getSearch(i) {
+  for(let i = 0; i < getAllEpisodes.length; i++)
+  if (getAllEpisodes[i].name.value == searchInput) {
+        index.style.display = "grid";
+      } else if (getAllEpisodes[i].summary.value == searchInput) {
+        index.style.display = "grid";
+      } else {
+        index.style.display = "none"
+      }
+      
+}
+
+// function showResults(query){
+//   const searchList = document.querySelector("#searchList");
+//   searchList.innerHTML = "";
+//   query.forEach((query) => {
+//       const li = document.createElement('li');
+//       li.innerText = query;
+//       searchList.append(li)
+//   })
+// }
+
